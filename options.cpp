db0f37a223ce8ddbbe2950c65c7aba8991cb02b8
Tvorba-Soft-Systemov <tvorba.soft.systemov@gmail.com>
2017-10-23 15:18:14 +0200

    parsing options with unit tests

--- options.cpp
+++ options.cpp
@@ -1,4 +1,6 @@
 #include "options.h"
+#include <string>
+
 
 std::optional<std::tuple<Order, Filter, Case, char *>> options::parse(int argc, char * argv[])
 {
@@ -7,7 +9,47 @@ std::optional<std::tuple<Order, Filter, Case, char *>> options::parse(int argc, 
 	Case compare { Case::sensitive };
 	char * input { nullptr };
 
-	// parse commandline options
+	 //parse commandline options
+	int pos = 1;
+	for (; pos < argc; ++pos)
+	{
+		std::string arg = argv[pos];
+		if (arg.empty() || arg[0] != '-')
+			break;
+
+		if (arg == "-u")
+		{
+			if (filter != Filter::all)
+				return {};
+			filter = Filter::unique;
+		}
+		else if (arg == "-r")
+		{
+			if (order != Order::ascending)
+				return {};
+			order = Order::descending;
+		}
+		else if (arg == "-i")
+		{
+			if (compare != Case::sensitive)
+				return {};
+			compare = Case::ignore;
+		}
+		else
+		{
+			return {};
+		}
+	}
+
+	if (pos < argc)
+	{
+		input = argv[pos++];
+	}
+
+	if (pos < argc)
+	{
+		return {};
+	}
 
 	return std::make_tuple(order, filter, compare, input);
 }
