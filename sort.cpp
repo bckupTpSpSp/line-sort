2789bee4a902b50d6097d5b8e1c7539b28147792
Tvorba-Soft-Systemov <tvorba.soft.systemov@gmail.com>
2017-11-06 16:18:16 +0100

    sorting - vector

--- sort.cpp
+++ sort.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <cctype>
 #include <sstream>
-
+#include <functional>
 
 namespace
 {
@@ -16,6 +16,27 @@ namespace
 	{
 		return std::getline(is, line);
 	}
+
+	struct NoCase
+	{
+		bool operator()(const std::string & a, const std::string & b) const
+		{
+			return _stricmp(a.c_str(), b.c_str()) < 0;
+		}
+	};
+
+	struct Reverse
+	{
+		template<typename Pred>
+		Reverse(Pred pred) : _predicate(pred) {}
+
+		bool operator()(const std::string & a, const std::string & b) const
+		{
+			return _predicate(b, a);
+		}
+	private:
+		std::function<bool(const std::string &, const std::string &)> _predicate;
+	};
 }
 
 bool sort::process(Order order, Filter filter, Case compare, std::istream & input, std::ostream & output)
@@ -23,6 +44,19 @@ bool sort::process(Order order, Filter filter, Case compare, std::istream & inpu
 	std::vector<std::string> lines { std::istream_iterator<Line>(input), std::istream_iterator<Line>() };
 
 	// implementation of sorting
+	std::function<bool(const std::string &, const std::string &)> fnOrder;
+	
+	if (compare == Case::ignore)
+		fnOrder = NoCase();
+	else
+		fnOrder = std::less<std::string>();
+
+	std::sort(lines.begin(), lines.end(), order == Order::ascending ? fnOrder : Reverse(fnOrder));
+
+	if (filter == Filter::unique)
+		lines.erase(std::unique(lines.begin(), lines.end(), [fnOrder](const std::string & a, const std::string & b) { return !fnOrder(a, b) && !fnOrder(b, a); }), lines.end());
+
+	std::copy(lines.begin(), lines.end(), std::ostream_iterator<std::string>(output, "\n"));
 
 	return true;
 }
