2b624ca54a27e0b38a8cbc9719e018dbd17316cf
Tvorba-Soft-Systemov <tvorba.soft.systemov@gmail.com>
2017-11-06 16:51:10 +0100

    solved - set/multiset

--- sort.cpp
+++ sort.cpp
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <iterator>
-#include <vector>
+#include <set>
 #include <algorithm>
 #include <cctype>
 #include <sstream>
@@ -16,13 +16,56 @@ namespace
 	{
 		return std::getline(is, line);
 	}
+
+	struct Sensitive
+	{
+		bool operator()(const std::string & a, const std::string & b) const { return a < b; }
+	};
+
+	struct NoCase
+	{
+		bool operator()(const std::string & a, const std::string & b) const { return _stricmp(a.c_str(), b.c_str()) < 0; }
+	};
+
+	template<typename Pred>
+	struct Reverse
+	{
+		bool operator()(const std::string & a, const std::string & b) const { return Pred()(b, a); }
+	};
+
+	template<typename Cont>
+	void Output(std::istream & input, std::ostream & output)
+	{
+		Cont lines { std::istream_iterator<Line>(input), std::istream_iterator<Line>() };
+
+		std::copy(lines.begin(), lines.end(), std::ostream_iterator<std::string>(output, "\n"));
+	}
+
+	template<typename Pred>
+	void ApplyFilter(Filter filter, std::istream & input, std::ostream & output)
+	{
+		if (filter == Filter::unique)
+			Output<std::set<std::string, Pred>>(input, output);
+		else
+			Output<std::multiset<std::string, Pred>>(input, output);
+	}
+
+	template<typename Pred> 
+	void ApplyOrder(Order order, Filter filter, std::istream & input, std::ostream & output)
+	{
+		if (order == Order::descending)
+			ApplyFilter<Reverse<Pred>>(filter, input, output);
+		else
+			ApplyFilter<Pred>(filter, input, output);
+	}
 }
 
 bool sort::process(Order order, Filter filter, Case compare, std::istream & input, std::ostream & output)
 {
-	std::vector<std::string> lines { std::istream_iterator<Line>(input), std::istream_iterator<Line>() };
-
-	// implementation of sorting
+	if (compare == Case::sensitive)
+		ApplyOrder<Sensitive>(order, filter, input, output);
+	else
+		ApplyOrder<NoCase>(order, filter, input, output);
 
 	return true;
 }
